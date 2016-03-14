bf42c5d6f172813c7da05ccd5436001255406141
zky <361561797@qq.com>
2016-03-14 17:43:46 +0800

    first commit

---
+++ app/models/publishing-house.js
@@ -0,0 +1,9 @@
+// import DS from 'ember-data';
+import Publisher from './publisher';
+export default Publisher.extend();
+
+// export default DS.Model.extend({
+//   name: DS.attr('string'),
+//   discount: DS.attr('number'),
+//   books: DS.hasMany('book')
+// });
